d70be2b51017b08c7bdda46bc9db22d668b5b69a
matejadb <92729211+matejadb@users.noreply.github.com>
2025-12-26 18:02:04 +0100

    feat: improve dropdown function and add tab switching

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,73 @@
-const categoryDropdownBtn = document.querySelector('.category-btn');
-const categoryDropdownContent = document.querySelector(
+/* GLOBAL */
+const categoryDropdownBtns = document.querySelectorAll('.category-btn');
+const categoryDropdownContents = document.querySelectorAll(
 	'.categories-dropdown--content'
 );
+const categoriesDropdowns = document.querySelectorAll('.categories-dropdown');
+const tablist = document.querySelector('.tablist');
+const studyModeBtn = document.querySelector('.tab--study-mode');
+const allCardsBtn = document.querySelector('.tab--all-cards');
+const studyMode = document.querySelector('.study-mode-content');
+const allCards = document.querySelector('.all-cards-content');
 
-categoryDropdownBtn.addEventListener('click', () => {
-	categoryDropdownContent.classList.toggle('hidden');
+/* FUNCTIONS */
+function selectStudyMode() {
+	tablist.querySelectorAll('button').forEach((btn) => {
+		btn.classList.remove('tab--current');
+		btn.setAttribute('aria-selected', false);
+	});
+
+	studyModeBtn.classList.add('tab--current');
+	studyModeBtn.setAttribute('aria-selected', true);
+
+	studyMode.classList.remove('hidden');
+	allCards.classList.add('hidden');
+}
+
+function selectAllCards() {
+	tablist.querySelectorAll('button').forEach((btn) => {
+		btn.classList.remove('tab--current');
+		btn.setAttribute('aria-selected', false);
+	});
+
+	allCardsBtn.classList.add('tab--current');
+	allCardsBtn.setAttribute('aria-selected', true);
+
+	allCards.classList.remove('hidden');
+	studyMode.classList.add('hidden');
+}
+
+function toggleCategoryDropdown(event) {
+	event.stopPropagation();
+	const btn = event.currentTarget;
+	const dropdown = btn.nextElementSibling;
+
+	dropdown.classList.toggle('hidden');
+	const isExpanded = !dropdown.classList.contains('hidden');
+	btn.setAttribute('aria-expanded', isExpanded);
+}
+
+function closeDropdowns(event) {
+	event.stopPropagation();
+	categoriesDropdowns.forEach((dropdown) => {
+		const dropdownContent = dropdown.querySelector(
+			'.categories-dropdown--content'
+		);
+		const dropdownBtn = dropdown.querySelector('.category-btn');
+		if (!dropdown.contains(event.target)) {
+			dropdownContent.classList.add('hidden');
+			dropdownBtn.setAttribute('aria-expanded', false);
+		}
+	});
+}
+
+/* EVENT LISTENERS */
+categoryDropdownBtns.forEach((btn) => {
+	btn.addEventListener('click', toggleCategoryDropdown);
 });
+
+document.addEventListener('click', closeDropdowns);
+
+studyModeBtn.addEventListener('click', selectStudyMode);
+
+allCardsBtn.addEventListener('click', selectAllCards);
